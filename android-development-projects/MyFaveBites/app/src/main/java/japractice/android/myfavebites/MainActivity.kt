c1803ad95699ceea0cceae5c36a4937bb909b4de
J. Anthony <janthonywriteseverything@gmail.com>
2023-04-14 22:06:11 -0400

    code practice: app idea to practice with constraints and remember key word

---
+++ android-development-projects/MyFaveBites/app/src/main/java/japractice/android/myfavebites/MainActivity.kt
@@ -0,0 +1,40 @@
+package japractice.android.myfavebites
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import japractice.android.myfavebites.ui.theme.MyFaveBitesTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyFaveBitesTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MyFaveBitesTheme {
+        Greeting("Android")
+    }
+}
