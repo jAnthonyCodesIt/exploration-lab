85f7249a7bdccc544a97abc89114504fa4f0a5d9
J. Anthony <janthonywriteseverything@gmail.com>
2023-04-14 21:27:46 -0400

    code: finished small project learning about states
    You can use the 'by' keyword with variables alongside the remember and rememberSaveable composables to save the state of a variable in an application. The 'remember' composable will update the state of a variable in real time. That memory lost, however, if the composables on screen experience recomposition. This is where 'rememberSaveable' comes in. It remembers state even after recomposition, like when the screen is rotated.

--- android-development-projects/StateCompose/app/src/main/java/udemy/android/statecompose/MainActivity.kt
+++ android-development-projects/StateCompose/app/src/main/java/udemy/android/statecompose/MainActivity.kt
@@ -3,13 +3,18 @@ package udemy.android.statecompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import udemy.android.statecompose.ui.theme.StateComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,7 +23,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             StateComposeTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+                Surface(color = MaterialTheme.colors.background) {
                     Greeting("Android")
                 }
             }
@@ -28,7 +33,30 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    var nameState by remember {
+        mutableStateOf("")
+    }
+    var name by rememberSaveable {
+        mutableStateOf("")
+    }
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize(),
+    ) {
+        Text("Hello $name")
+        Spacer(modifier = Modifier.height(20.dp))
+        TextField(value = nameState, onValueChange = {
+            nameState = it
+        })
+        Spacer(modifier = Modifier.height(20.dp))
+        Button(onClick = {
+            name = nameState
+        }) {
+            Text(text ="Display")
+        }
+    }
 }
 
 @Preview(showBackground = true)
